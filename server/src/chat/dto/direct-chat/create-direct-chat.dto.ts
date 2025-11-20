65984ac1dd2cb4f6945334e3715b09e7b48963f6
EzioAuditore12 <dakshpurohit2005@gmail.com>
2025-11-20 09:00:16 +0530

    -a

--- server/src/chat/dto/direct-chat/create-direct-chat.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { MaxLength } from 'class-validator';
-
-export class CreateDirectChatDto {
-  @ApiProperty({ type: 'string', maxLength: 1000 })
-  @MaxLength(1000)
-  text: string;
-}
